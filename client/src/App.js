0101bb8c7100d283476bd86242887297f8cda190
Chase Teague <106894422+ChaseTeague1@users.noreply.github.com>
2025-08-11 10:37:01 -0400

    setup basic client side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
+import BrowseRecipe from "./pages/BrowseRecipe";
+import PostRecipe from "./pages/PostRecipe";
+import LoginSignup from "./pages/LoginSignup";
 
+
+console.log("React version:", React.version);
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/browse" element={<BrowseRecipe />} />
+        <Route path="/post" element={<PostRecipe />} />
+        <Route path="/login" element={<LoginSignup />} />
+        {/* Optional 404 page */}
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-
-function NavBar(){
-    return (
-        <div>
-            <h1>NavBar</h1>
-        </div>
-    )
+function Navbar() {
+  return (
+    <nav>
+      <h2>Recipe Finder</h2>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/browse">Browse Recipes</Link></li>
+        <li><Link to="/post">Post a Recipe</Link></li>
+        <li><Link to="/login">Login / Signup</Link></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default NavBar;
+export default Navbar;
