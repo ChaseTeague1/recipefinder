ef6771642fd677c0f92e50a05af9a09ad04abc8f
Chase Teague <106894422+ChaseTeague1@users.noreply.github.com>
2025-08-21 16:47:38 -0400

    making sure everything is up to date on all systems

--- client/src/pages/PostRecipe.js
+++ client/src/pages/PostRecipe.js
@@ -3,8 +3,21 @@ import React from "react";
 
 function PostRecipe(){
     return (
-        <div>
-            <h1>PostRecipe page</h1>
+        <div className="post-recipe-container">
+            <h1>Add your own recipe!</h1>
+            <label>Title</label>
+            <input />
+            <label>Time</label>
+            <input />
+            <label>Description</label>
+            <textarea />
+            <label>Ingredients</label>
+            <textarea />
+            <label>Instructions</label>
+            <textarea />
+            <label>Image</label>
+            <input />
+            <button>Add Recipe!</button>
         </div>
     )
 }
