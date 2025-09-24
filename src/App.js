083238672d172f0c7e084d4ae3b16a7ccdc78392
Chase Teague <106894422+ChaseTeague1@users.noreply.github.com>
2025-09-24 14:32:00 -0400

    added recipe delete function to the backend and frontend

--- src/App.js
+++ src/App.js
@@ -22,12 +22,24 @@ function App() {
       setRecipes([...recipes, newRecipe])
     }
 
+    function deleteRecipe(id){
+      fetch(`/recipes/${id}`, {
+        method: 'DELETE',
+      })
+      .then(res => {
+        if (res.ok){
+          setRecipes((recipes) => recipes.filter(recipe => recipe.id !== id))
+        }
+      })
+
+    }
+
   return (
     <div className="app-container">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home recipes={recipes}/>} />
-        <Route path="/browse" element={<BrowseRecipe recipes={recipes}/>} />
+        <Route path="/browse" element={<BrowseRecipe handleDelete={deleteRecipe} recipes={recipes}/>} />
         <Route path="/post" element={<PostRecipe onRecipeSubmit={onRecipeSubmit}/>} />
         <Route path="/login" element={<LoginSignup />} />
         {/* Optional 404 page */}
--- src/pages/BrowseRecipe.js
+++ src/pages/BrowseRecipe.js
@@ -2,12 +2,15 @@ import React from "react";
 import RecipeCard from "../components/RecipeCard";
 
 
-function BrowseRecipe({recipes}){
+function BrowseRecipe({recipes, handleDelete}){
     return (
         <div className="browse-recipe-div">
             {
                 recipes.map(recipe => (
-                    <RecipeCard key={recipe.id} recipe={recipe}/>
+                    <div key={recipe.id}>
+                        <RecipeCard recipe={recipe}/>
+                        <button onClick={() => handleDelete(recipe.id)}>Delete</button>
+                    </div>
                 ))
             }
         </div>
