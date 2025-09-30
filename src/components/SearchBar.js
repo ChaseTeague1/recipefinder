5cbef559a1c4a17e2948a73e3d30ef51b6517f61
Chase Teague <106894422+ChaseTeague1@users.noreply.github.com>
2025-09-30 14:19:01 -0400

    Linked search recipes button to navigate to the browse recipe page and added confirmation to recipe delete button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import BrowseRecipe from "../pages/BrowseRecipe";
 
 
 function SearchBar(){
 
     return (
         <div>
+            <Link to={'/browse'}>
             <h2 className="search-name">Search Recipes</h2>
+            </Link>
         </div>
     )
 }
--- src/pages/BrowseRecipe.js
+++ src/pages/BrowseRecipe.js
@@ -4,13 +4,20 @@ import {FaTrash} from 'react-icons/fa'
 
 
 function BrowseRecipe({recipes, handleDelete}){
+
+    const confirmDelete = (id) => {
+        if (window.confirm('Are you sure you want to delete this recipe?')){
+            handleDelete(id)
+        }
+    }
+
     return (
         <div className="browse-recipe-div">
             {
                 recipes.map(recipe => (
                     <div className="card-containers" key={recipe.id}>
                         <RecipeCard recipe={recipe}/>
-                        <button className="delete-btn" onClick={() => handleDelete(recipe.id)}>
+                        <button className="delete-btn" onClick={() => confirmDelete(recipe.id)}>
                             <FaTrash />
                             <span className="tooltip">Delete</span>
                         </button>
