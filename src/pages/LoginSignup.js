7c6c06089dc9733469bcfd51c4ce1f30b502ee9a
Chase Teague <106894422+ChaseTeague1@users.noreply.github.com>
2025-10-01 16:33:19 -0400

    started trying to find a layout for the login/signup page

--- src/pages/LoginSignup.js
+++ src/pages/LoginSignup.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { BiColor } from "react-icons/bi";
 
 
 function LoginSignup(){
     return (
-        <div>
-            <h1>LoginSignup Page</h1>
+        <div className="post-recipe-container">
+            <h1>Login</h1>
+            <div>
+                <input placeholder="Username"/>
+                <input placeholder="Email"/>
+                <p>dont have an account? <a>Sign up!</a></p>
+            </div>
+            <button>Login</button>
         </div>
     )
 }
